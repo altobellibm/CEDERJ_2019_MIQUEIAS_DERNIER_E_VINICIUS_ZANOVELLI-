1098df7be996a81cf1728ecc60daa3322fea4b4f
Vinicius Zanovelli <vin@znvl.com>
2019-10-31 05:43:15 -0300

    Removing POC demo and examples (since POC gave place to implementation)

--- implementation/applications/demo/input/api_get.py
+++
@@ -1,3 +0,0 @@
-def api_get(api):
-    page = api.get('http://uff.br/')
-    api.log('Page title is: %s' % page.title.text)
--- implementation/applications/demo/input/api_start_browser.py
+++
@@ -1,8 +0,0 @@
-def api_start_browser(api):
-    url = 'https://www.mozilla.org'
-    ff = api.start_browser('Firefox')
-    ff.get(url)
-    page_title = ff.title
-    ff.quit()
-    api.log('GET %s' % url)
-    api.log('Page title is: %s' % page_title)
