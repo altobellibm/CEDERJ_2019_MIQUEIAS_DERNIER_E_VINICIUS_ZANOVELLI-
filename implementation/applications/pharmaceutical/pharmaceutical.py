40f6630acbfe76dabc7bc92e0843ecac14405681
Vinicius Zanovelli <vin@znvl.com>
2019-10-31 16:58:08 -0300

    Updating "chromedriver" version (removing 76, adding 78, keeping 77)

--- implementation/applications/pharmaceutical/pharmaceutical.py
+++ implementation/applications/pharmaceutical/pharmaceutical.py
@@ -36,10 +36,7 @@ scrapper = Pharmaceutical(dict(
         logs='./logs/',
         drivers=dict(
             geckodriver='../../vendor/geckodriver/0.24.0/',             # Firefox 65+
-            chromedriver='../../vendor/chromedriver/76.0.3809.68/',     # Chrome 76
-            # chromedriver='../../vendor/chromedriver/77.0.3865.10/',   # Chrome 77
-            # safaridriver='',  # no support for Safari at this time
-            # msdriver='',      # no support for Edge at this time
+            chromedriver='../../vendor/chromedriver/78.0.3904.70/',     # Chrome 78
         ),
     ),
     verbose=True,
